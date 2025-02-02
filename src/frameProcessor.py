ccb0382e816c3ecf7f5e07ff39296baffe2f7ba1
Andrii Hrytsenko <73461908+anryts@users.noreply.github.com>
2025-02-02 19:11:49 +0200

    updated output window with class and confidence (%) for prediction

--- src/frameProcessor.py
+++ src/frameProcessor.py
@@ -20,13 +20,14 @@ class FrameProcessor:
         if frame.shape[2] == 4:  # If the frame has an alpha channel (RGBA)
             frame = cv2.cvtColor(frame, cv2.COLOR_BGRA2BGR)  # Convert to BGR (3 channels)
 
-        results = self.model(frame, device="cuda")  # Run YOLO inference
-
+        results = self.model(frame, device="cuda", stream=True)  # Run YOLO inference
         for result in results:
-            for box in result.boxes.xyxy:
+            for box, cls, conf in zip(result.boxes.xyxy, result.boxes.cls, result.boxes.conf):
                 x1, y1, x2, y2 = map(int, box[:4])  # Extract bounding box coordinates
+                label = self.model.names[int(cls)]  # Extract label of box
+                frame_text = f"{label}: {conf:.2f}"
                 cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 255, 0), 2)  # Draw bounding box
-
+                cv2.putText(frame, frame_text, (x1, y1 - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 255), 2)
         return frame
 
     def close(self):
